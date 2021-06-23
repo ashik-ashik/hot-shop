b574941afddd6ef55c2996aea91d40f0b98a8e97
Md Ashik Ali <mdashika989@yahoo.com>
2021-06-24 00:26:35 +0600

    fix counter

--- js/cart.js
+++ js/cart.js
@@ -53,26 +53,27 @@ $(document).ready(function(){
 
 
     // about page counter
-
+    $('.counter').each(function(){
+      $(this).prop("Counter", 0).animate({
+        Counter: $(this).text()
+      },
+      {
+        duration: 3500,
+        easing: 'swing',
+        step: function(now){
+          $(this).text(Math.ceil(now)+'+')
+        }
+      }
+      )
+    });
+    
   $(window).scroll(function(){
     let CounterPosition = $(document).scrollTop();
     console.log(CounterPosition);
 
     if(CounterPosition > 1990){
 
-      $('.counter').each(function(){
-        $(this).prop("Counter", 0).animate({
-          Counter: $(this).text()
-        },
-        {
-          duration: 3500,
-          easing: 'swing',
-          step: function(now){
-            $(this).text(Math.ceil(now)+'+')
-          }
-        }
-        )
-      });
+    
 
     }
   })
